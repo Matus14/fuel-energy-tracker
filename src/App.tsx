315a6064fc110ed6b90f7ba9ce1071479a78eb0a
Matus <matus1142@gmail.com>
2025-08-18 20:34:01 +0200

    initial commit Vite React + TypeScript setup with Bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="container py-5">
+      <h1 className="mb-4">Fuel & Energy Tracker</h1>
+      <p className="text-muted">
+        This is our starting point. 
+      </p>
+
+      {/* Example of a Bootstrap button */}
+      <button className="btn btn-primary">
+        Hello Voltia 
+      </button>
+    </div>
+  )
+}
+
+export default App
