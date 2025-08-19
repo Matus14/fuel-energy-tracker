c103fde5465f70776b68302dda730d8e3d6d182b
Matus <matus1142@gmail.com>
2025-08-19 14:21:26 +0200

    add explanatiuon/comments for each block od code

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,62 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { loadLogs, saveLogs } from './storage';
 import type { TripLog } from './types';
 import './App.css'
 
 function App() {
-  
+
+  // in useState must be define the type = TripLog
   const [logs, setLogs] = useState<TripLog[]>([]);
 
-  useEffect (() => {
+
+  // useEffect with [] (empty array)
+  // Run this only once,  after the component shows up on screen
+  // In here its load any saved logs from localStorage
+  useEffect(() => {
     setLogs(loadLogs());
   }, []);
 
+  
+  // Another useEffect, but this one "watches" the logs array
+  // Whenever logs changes ( when added  or removed something), this effect runs and save logs
+ 
   useEffect(() => {
     saveLogs(logs);
   }, [logs]);
-  
+
+ 
+
+  // This creates how the UI should look like
+  // React will render this JSX into HTML
   return (
-    <div>
-      <h1>Fuel & Energy Tracker</h1>
+    <div className='container py-4'>
+      <h1 className='mb-4'>Fuel & Energy Tracker</h1>
 
       <p> Logs stored in LocalStorage:</p>
 
-      <ul>
+      {/* This part loops over the logs array and prints each log as <li>. 
+          The "key" prop is super important in React lists
+          so React can keep track of items when re-rendering. */}
+      <ul className='list-group'>
         {logs.map((log) =>
-        <li key={log.id}>
-          {log.date} - {log.vehicleName} ({log.vehicleType} - {log.distanceKm}) km
-        </li>)}
+          <li key={log.id} className='list-group-item'>
+            {log.date} - {log.vehicleName} ({log.vehicleType} - {log.distanceKm} km) 
+          </li>
+        )}
       </ul>
 
+      {/* A button styled with Bootstrap classes.
+          It creates new TripLog object
+          with some dummy values (like distanceKm = 42)
+          Then it updates the state with setLogs.
+          Notice the syntax: setLogs((prev) => [newLog, ...prev]).
+          That means: take the new log and put it at the front of the array,
+          then spread all the previous logs after it.
+          React will re-render, the UI updates, and thanks to our useEffect,
+          the new log also gets saved into localStorage. */}
       <button
         className="btn btn-primary mt-3"
         onClick={() => {
-          // For now, just add dummy log to test saving
           const newLog: TripLog = {
             id: Math.random().toString(36).slice(2, 9),
             date: new Date().toISOString().slice(0, 10),
@@ -47,23 +72,8 @@ function App() {
       >
         Add dummy log
       </button>
-
     </div>
   )
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
- 
 }
 
 export default App
